5ee3ba6645ce7196c5b5ed2e5cfe791271dd45e4
NguyenThiNgocTram122 <ngoctram201715@gmail.com>
2023-12-26 10:41:22 +0700

    baithi

---
+++ thicuoiky/thicuoiky/Danhsachtruonghoc.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" MasterpageFile="~/Masterpage.Master" CodeBehind="Danhsachtruonghoc.aspx.cs" Inherits="thicuoiky.Danhsachtruonghoc" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+     <div>
+                 <asp:Label ID="Label3" runat="server" Text="DANH SÁCH TRƯỜNG HỌC "></asp:Label>
+            </div>
+           <div>
+               <asp:Label ID="Label2" runat="server" Text="Mã trường"></asp:Label>
+               <asp:TextBox ID="txtmatruong" runat="server"></asp:TextBox>
+           </div>
+            <div>
+                <asp:Label ID="Label4" runat="server" Text="Tên trường"></asp:Label>
+                <asp:TextBox ID="txttentruong" runat="server"></asp:TextBox>
+            </div>
+            <div>
+                <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" AllowPaging="True" OnPageIndexChanging="GridView1_PageIndexChanging" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" PageSize="3">
+                    <Columns>
+                        <asp:BoundField DataField="MaTruong" HeaderText="Mã trường " />
+                        <asp:BoundField DataField="TenTruong" HeaderText="Tên trường" />
+                        <asp:CommandField SelectText="Chọn" ShowSelectButton="True" />
+                    </Columns>
+                </asp:GridView>
+            </div>
+            <div>
+                <asp:Label ID="lbtthongbao" runat="server" Text="Label"></asp:Label>
+            </div>
+            <div>
+                <asp:Button ID="btnthem" runat="server" Text="Thêm" OnClick="btnthem_Click" />
+                <asp:Button ID="btnluu" runat="server" Text="Lưu" OnClick="btnluu_Click" />
+                <asp:Button ID="btnsua" runat="server" Text="Sửa" OnClick="btnsua_Click" />
+                <asp:Button ID="btnxoa" runat="server" Text="Xóa" OnClick="btnxoa_Click" />
+            </div>
+</asp:Content>
---
+++ thicuoiky/thicuoiky/Danhsachtruonghoc.aspx.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace thicuoiky
+{
+    public partial class Danhsachtruonghoc : System.Web.UI.Page
+    {
+       
+             public static string tram = "Data Source=LAPTOP-00N9N4JN\\SQLEXPRESS;Initial Catalog=QL_SINHVIEN;Integrated Security=True";
+        public static SqlConnection cn = new SqlConnection(tram);
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HienThi();
+        }
+        void HienThi()
+        {
+            try
+            {
+                string chuoiSQL = "SELECT * FROM tbl_truong";
+                SqlDataAdapter da = new SqlDataAdapter(chuoiSQL, cn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                GridView1.DataSource = dt;
+
+                GridView1.DataBind();
+            }
+            catch (Exception)
+            {
+                lbtthongbao.Text = "Lỗi kết nối";
+            }
+        }
+        void ThucThi(string caulenh)
+        {
+            SqlCommand cm = new SqlCommand(caulenh, cn);
+            cn.Open();
+            cm.ExecuteNonQuery();
+            cn.Close();
+        }
+        Boolean kiemtra(string caulenh)
+        {
+            SqlDataAdapter sql = new SqlDataAdapter(caulenh, cn);
+            DataTable data = new DataTable();
+            sql.Fill(data);
+            if (data.Rows.Count > 0)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+        protected void btnthem_Click(object sender, EventArgs e)
+        {
+            txtmatruong.Text = "";
+            txttentruong.Text = "";
+        }
+        protected void btnluu_Click(object sender, EventArgs e)
+        {
+            string khaibao = "select * from tbl_truong where MaTruong = '" + txtmatruong.Text + "' or TenTruong = N'" + txttentruong.Text + "'";
+
+            if (kiemtra(khaibao))
+            {
+
+                lbtthongbao.Text = "Tên môn học đã tồn tại";
+            }
+            else
+            {
+
+                string them = "insert into tbl_truong values ('" + txtmatruong.Text + "' , N'" + txttentruong.Text + "')";
+                ThucThi(them);
+                HienThi();
+            }
+        }
+        protected void btnsua_Click(object sender, EventArgs e)
+        {
+            string sua = "update tbl_truong set TenTruong = N'" + txttentruong.Text + "' where MaTruong = '" + txtmatruong.Text + "'";
+            ThucThi(sua);
+            HienThi();
+        }
+        protected void btnxoa_Click(object sender, EventArgs e)
+        {
+            string truyen = "delete tbl_truong where MaTruong = '" + txtmatruong.Text + "'";
+            ThucThi(truyen);
+            HienThi();
+            txtmatruong.Text = "";
+            txttentruong.Text = "";
+        }
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+        }
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string ngoctram = "select * from tbl_truong";
+            SqlDataAdapter sqldata = new SqlDataAdapter(ngoctram, cn);
+            DataTable dttable = new DataTable();
+            sqldata.Fill(dttable);
+            int dong = GridView1.SelectedIndex;
+            int page = GridView1.PageIndex;
+            txtmatruong.Text = dttable.Rows[page * 3 + dong][0].ToString();
+            txttentruong.Text = dttable.Rows[page * 3 + dong][1].ToString();
+        }
+
+    }
+}
+    
